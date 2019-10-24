4987a3756cab65bc23870c4b2ca97ce29ff7e686
wangquangang <tcfgarywang@gmail.com>
2019-10-24 13:32:45 +0800

    creat

---
+++ QUICKLEARN/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QUICKLEARN
+//
+//  Created by  yangfuke on 2019/10/24.
+//  Copyright © 2019 wangquangang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
